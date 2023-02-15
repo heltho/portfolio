7922ad6d6b8f3aa0c32d0986443fd4de2bbf4e07
Thomas Hellin <thomas.hellin@hotmail.com>
2023-02-15 22:41:31 +0100

    feat: add button component

---
+++ components/atoms/Button.tsx
@@ -0,0 +1,19 @@
+interface ButtonProps {
+  label: string;
+  link?: boolean;
+  href?: string;
+}
+
+type ButtonTag = "a" | "button";
+
+export default function Button({ label, link = true, href }: ButtonProps) {
+  let BtnType = link ? "a" : ("button" as ButtonTag);
+  return (
+    <BtnType
+      href={href}
+      className="flex px-4 py-2 text-xl transition bg-scooter text-dark w-max hover:bg-scooter-400"
+    >
+      {label}
+    </BtnType>
+  );
+}
