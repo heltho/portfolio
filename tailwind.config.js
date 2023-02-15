d441873d4e885271da600d2ed8fae841660d04bc
Thomas Hellin <thomas.hellin@hotmail.com>
2023-02-15 22:41:11 +0100

    style: add colors to TW

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        scooter: {
+          DEFAULT: "#32C4D8",
+          50: "#D6DFFF",
+          100: "#BBEBF2",
+          200: "#99E2EC",
+          300: "#77D8E5",
+          400: "#54CEDF",
+          500: "#32C4D8",
+          600: "#229FB0",
+          700: "#197581",
+          800: "#104A52",
+          900: "#072023",
+        },
+        "white-alt": "#DFDFDF",
+        dark: "#2F333D",
+        light: "#363C4A",
+      },
+    },
   },
   plugins: [],
 };
